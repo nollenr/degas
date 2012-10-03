59ef870453b8241c6592fdd573331f7c5c93264d
user name <nameu@unlv.edu>
2012-10-03 11:46:23 -0700

    Seed bottles table

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,31 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-puts "Loading grapes..."
+#  puts "Loading grapes..."
 
-Grape.delete_all
-open("#{Rails.root}/db/redwinegrapes.csv") do |grapefile|
-  grapefile.read.each_line do |grapeline|
-    grape, color = grapeline.chomp.split(",")
-    puts "Creating new grape: #{grape}"
-    Grape.create!(name: grape, color: color)
+#  Grape.delete_all
+#  open("#{Rails.root}/db/redwinegrapes.csv") do |grapefile|
+#    grapefile.read.each_line do |grapeline|
+#      grape, color = grapeline.chomp.split(",")
+#      puts "Creating new grape: #{grape}"
+#      Grape.create!(name: grape, color: color)
+#    end
+#  end
+
+#NOTE:  Must add 
+#  bottle.rb model
+#  attr_accessible :bottle_id, :grape_id
+#
+#  remove ":grape_id" when load is complete
+
+puts "Loading bottles..."
+
+Bottle.delete_all
+open("#{Rails.root}/db/bottles.csv") do |bottlefile|
+  bottlefile.read.each_line do |bottleline|
+    v_bottle_id, v_grape_id = bottleline.chomp.split(",")
+    puts "Creaing new bottle... bottle_id: #{v_bottle_id} grape_id: #{v_grape_id}"
+    Bottle.create!(bottle_id: v_bottle_id, grape_id: v_grape_id)
   end
 end
 
