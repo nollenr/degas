4cc0260f07aba25c821b132cb5742c6a607307f1
user name <nameu@unlv.edu>
2012-10-04 13:54:19 -0700

    Successfully added a flash message when a bottle is consumed.

--- app/controllers/bottle_controller.rb
+++ app/controllers/bottle_controller.rb
@@ -2,12 +2,13 @@ class BottleController < ApplicationController
 
   def index
     # @bottles = Bottle.find :all, select: "*, case when available then 'Available' else 'Consumed' end as availability", order: "bottle_id"
-    @bottles = Bottle.all
+    @bottles = Bottle.order("bottle_id").all
   end
 
 	def consume
     bottle = Bottle.update(params[:id], available: :false)
     bottle.save
+    flash[:success] = "You have successfully consumed bottle " + bottle.bottle_id.to_s + "!"
     redirect_to bottle_index_path
   end
 
