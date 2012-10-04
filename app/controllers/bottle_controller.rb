81955b262d8ff168055b1228d04abe159ba63219
user name <nameu@unlv.edu>
2012-10-04 13:19:50 -0700

    Removed the 'select' clause from the find command.  Added an 'after_find' to accomplish the action.  Not sure how it works exactly... I think I'm getting a second hash... active record has one... and now I have another one.

--- app/controllers/bottle_controller.rb
+++ app/controllers/bottle_controller.rb
@@ -1,7 +1,8 @@
 class BottleController < ApplicationController
 
   def index
-    @bottles = Bottle.find :all, select: "*, case when available then 'Available' else 'Consumed' end as availability", order: "bottle_id"
+    # @bottles = Bottle.find :all, select: "*, case when available then 'Available' else 'Consumed' end as availability", order: "bottle_id"
+    @bottles = Bottle.all
   end
 
 	def consume
--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -1,4 +1,17 @@
 class Bottle < ActiveRecord::Base
   belongs_to :grape
-  attr_accessible :bottle_id, :available
+  attr_accessible :bottle_id, :available, :availability
+
+  after_find :set_availability
+
+  def set_availability
+    if self.available 
+			#puts "And the bottle is available"
+      self[:availability] =  'Available'
+    else
+      #puts "And the bottle is UNavailable"
+      self[:availability] =  'Consumed'
+    end
+  end
+
 end
