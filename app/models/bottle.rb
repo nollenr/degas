1e421829b0bd168d3425f9a8947e727a4b549fb1
nollenr2 <nollenr@me.com>
2012-10-06 13:43:17 -0700

    Added a setter/getter for the 'availability' attribute.

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -2,18 +2,20 @@ class Bottle < ActiveRecord::Base
   belongs_to :grape
   # In order to run seed, grape_id needs to be accessible
   # attr_accessible :bottle_id, :available, :availability, :grape_id
-  attr_accessible :bottle_id, :available
+  attr_accessible :available
+
+  def availability
+    return @availability
+  end
+
+  def availability= avail_bool
+    @availability = avail_bool ? 'Available' : 'Consumed'
+  end
 
   after_find :set_availability
 
   def set_availability
-    if self.available 
-			#puts "And the bottle is available"
-      self[:availability] =  'Available'
-    else
-      #puts "And the bottle is UNavailable"
-      self[:availability] =  'Consumed'
-    end
+    self.availability= self.available
   end
 
 end
