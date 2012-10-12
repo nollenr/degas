beabf7babec0e27d85030dc97627ea7261f615f6
user name <nameu@unlv.edu>
2012-10-12 13:14:56 -0700

    Some code clean-up, including elimination of variables.  AJAX 'available' toggle now includes table header.

--- app/controllers/bottle_controller.rb
+++ app/controllers/bottle_controller.rb
@@ -3,17 +3,13 @@ class BottleController < ApplicationController
 
   def index
 
-    @before_changes = params[:show_avail_next]
-    @params_has_key = params.has_key?(:show_avail_next)
     v_find_hash = {}
     v_find_hash[:include] = [:grape, :winery]
     v_find_hash[:order] = sort_column + " " + sort_direction
-    if params[:show_avail_next] == 'true'
+    if show_avail == 'true'
       v_find_hash[:conditions] = {available: true} 
     end
     @bottles = Bottle.all(v_find_hash)
-
-    @after_changes = params[:show_avail_next]
 
     respond_to do |format|
       format.html #index.html.erb
