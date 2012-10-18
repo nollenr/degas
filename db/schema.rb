10dfb2f0ffb4f527ed19dbe2619c72038314e739
user name <nameu@unlv.edu>
2012-10-18 13:09:41 -0700

    Working db migration for the trigger function and the trigger on bottles (available).

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121012235303) do
+ActiveRecord::Schema.define(:version => 20121018200136) do
 
   create_table "bottles", :force => true do |t|
     t.integer  "bottle_id"
