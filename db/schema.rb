0f13139818e23a95e5c0c0e35fc932a3573292b1
nollenr2 <nollenr@me.com>
2013-02-03 10:45:32 -0800

    Added foreign key from bottles to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130203171225) do
+ActiveRecord::Schema.define(:version => 20130203183910) do
 
   create_table "bottle_types", :force => true do |t|
     t.string   "name",          :limit => 30, :null => false
