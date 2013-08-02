55ac399674c70b86e90b2766d2a82ec28b8dd39f
Ron <nollenr@gmail.com>
2013-08-02 15:44:57 -0700

    Updated version

--- config/initializers/app_verion.rb
+++ config/initializers/app_verion.rb
@@ -1,3 +1,3 @@
 # This file (config/initializers/app_version.rb) created by rake generate_globals (lib/tasks/genereate_globals.rake)
-# Created : 2013-08-02 15:21:03 -0700
-APPVERSION="v1.3-13-gaab4b21"
+# Created : 2013-08-02 15:44:37 -0700
+APPVERSION="v1.3-17-g86dc759"
