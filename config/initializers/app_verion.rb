417d3c18e6b4cd546376eb7b9010790298240b29
Ron <nollenr@gmail.com>
2013-12-10 14:12:51 -0800

    generated globals

--- config/initializers/app_verion.rb
+++ config/initializers/app_verion.rb
@@ -1,3 +1,3 @@
 # This file (config/initializers/app_version.rb) created by rake generate_globals (lib/tasks/genereate_globals.rake)
-# Created : 2013-12-04 13:39:51 -0800
-APPVERSION="v1.4-8-g09109eb44b"
+# Created : 2013-12-10 14:12:42 -0800
+APPVERSION="v1.5.2-0-g55adebb853"
