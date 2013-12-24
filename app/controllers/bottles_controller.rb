4618c02539f4da2107b0b5c2c1cddb27a96f73c1
Ron <nollenr@gmail.com>
2013-12-24 08:02:39 -0800

    Consume modal displays the 'disposition reason' section.  The new/edit bottle page also displays the 'disposition reason' section, but only when the availability is set to false (and the bottle is not a 'rating only' bottle.  The show/hide of the disposition section on the edit bottle page is handled through css and JavaScript

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -155,9 +155,9 @@ class BottlesController < ApplicationController
     # @bottles = @bottles.where(is_for_rating_only: false) unless params[:q] && params[:q][:is_for_rating_only_true]
     @bottles = @bottles.page(params[:page]).per(15)
     #logger.debug "*****************   End of controller"
-    # create a collection of availibility_change_reasons
+    # create a collection of availibility_change_reasons - this will get displayed on the "consume pop-up" modal
     @availability_change_reasons = AvailabilityChangeReasonLookup.select("id, reason").order("display_order")
-    logger.debug "******************   Availability Change Reasons: #{@availability_change_reasons.inspect}"
+    # logger.debug "******************   Availability Change Reasons: #{@availability_change_reasons.inspect}"
     respond_to do |format|
       format.html #index.html.erb
       format.js   #index.js.erb
@@ -213,6 +213,8 @@ class BottlesController < ApplicationController
 
   def edit
     @bottle = current_user.bottles.find_by_id(params[:id])
+    # create a collection of availibility_change_reasons
+    @availability_change_reasons = AvailabilityChangeReasonLookup.select("id, reason").order("display_order")
     render 'new'
   end
 
--- db/migrate/20131220230339_availability_change_reason_seed_data.rb
+++ db/migrate/20131220230339_availability_change_reason_seed_data.rb
@@ -3,7 +3,8 @@ class AvailabilityChangeReasonSeedData < ActiveRecord::Migration
     AvailabilityChangeReasonLookup.create(reason: "Consumed", display_order: 10)
     AvailabilityChangeReasonLookup.create(reason: "Spoiled", display_order: 20)
     AvailabilityChangeReasonLookup.create(reason: "Gift", display_order: 30)
-    AvailabilityChangeReasonLookup.create(reason: "Other", display_order: 40)
+    AvailabilityChangeReasonLookup.create(reason: "Lost", display_order: 40)
+    AvailabilityChangeReasonLookup.create(reason: "Other", display_order: 50)
   end
 
   def down
