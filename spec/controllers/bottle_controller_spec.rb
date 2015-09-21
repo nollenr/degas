5a2cd1314f8238232108a22b3cfc7d48b0a805d2
nollenr <nollenr@renoir.(none)>
2015-09-21 12:43:25 -0700

    Preparing for Degas 2.0

--- spec/controllers/bottle_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe BottleController do
-
-end
--- spec/models/availability_change_reason_lookup_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe AvailabilityChangeReasonLookup do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
